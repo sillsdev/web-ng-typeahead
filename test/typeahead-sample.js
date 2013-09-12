1140639d66126600a0d1127cb24f797eb9ef7e1b
cambell-prince <cambell.prince@gmail.com>
2013-09-12 11:05:45 +0700

    Added images to sample.

--- test/typeahead-sample.js
+++ test/typeahead-sample.js
@@ -2,19 +2,19 @@
 angular.module('palaso.sample.typeahead', ['palaso.ui.typeahead'])
 	.controller('TypeaheadSampleCtrl', ['$scope', function($scope) {
 		$scope.things = [
-             { name: 'Bob',     email: 'bob@example.com' },
-             { name: 'Bobby',   email: 'bobby@example.com'  },
-             { name: 'Robert',  email: 'robert@example.com'  },
-             { name: 'Roberta', email: 'roberta@example.com'  },
-             { name: 'Bert',    email: 'bert@example.com'  },
-             { name: 'Wendy',   email: 'wendy@example.com'  },
-             { name: 'Pickles', email: 'pickles@example.com'  },
-             { name: 'Scoop',   email: 'scoop@example.com'  },
-             { name: 'Muck',    email: 'muck@example.com'  },
-             { name: 'Dizzy',   email: 'dizzy@example.com'  },
-             { name: 'Roley',   email: 'roley@example.com'  },
-             { name: 'Lofty',   email: 'lofty@example.com'  },
-             { name: 'Travis',  email: 'travis@example.com'  },
+             { name: 'Bob',     email: 'bob@example.com',     image: 'm3.jpg' },
+             { name: 'Bobby',   email: 'bobby@example.com',   image: 'm2.jpg' },
+             { name: 'Robert',  email: 'robert@example.com',  image: 'm1.jpg' },
+             { name: 'Roberta', email: 'roberta@example.com', image: 'f1.jpg' },
+             { name: 'Bert',    email: 'bert@example.com',    image: 'm1.jpg' },
+             { name: 'Wendy',   email: 'wendy@example.com',   image: 'f2.jpg' },
+             { name: 'Pickles', email: 'pickles@example.com', image: 'm1.jpg' },
+             { name: 'Scoop',   email: 'scoop@example.com',   image: 'c1.jpg' },
+             { name: 'Muck',    email: 'muck@example.com',    image: 'c1.jpg' },
+             { name: 'Dizzy',   email: 'dizzy@example.com',   image: 'c1.jpg' },
+             { name: 'Roley',   email: 'roley@example.com',   image: 'c1.jpg' },
+             { name: 'Lofty',   email: 'lofty@example.com',   image: 'c1.jpg' },
+             { name: 'Travis',  email: 'travis@example.com',  image: 'c1.jpg' },
         ];
 		
 		$scope.typeahead = {};
@@ -36,6 +36,10 @@ angular.module('palaso.sample.typeahead', ['palaso.ui.typeahead'])
 		$scope.selectUser = function(user) {
 			console.log('selectUser: ', user);
 			$scope.typeahead.userName = user.name;
+		};
+		
+		$scope.imageSource = function(image) {
+			return '../src/images/' + image;
 		};
 		
 	}])
